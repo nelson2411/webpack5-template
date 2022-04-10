c142a619951d222865f3c1ffcd9ee3cecd24135d
Nelson Rosales <nelson.rosales83@gmail.com>
2022-04-10 13:27:51 +0200

    Project finished

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,5 +1,16 @@
+import axios from "axios";
+
 function generateJoke() {
-  return "I don't know any jokes";
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  axios.get("https://icanhazdadjoke.com/", config).then((res) => {
+    document.getElementById("joke").innerHTML = res.data.joke;
+    console.log(res.data.joke);
+  });
 }
 
 export default generateJoke;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import generateJoke from "./generateJoke";
+import "./styles/main.scss";
+import laughing from "./assets/laugh-icon.svg";
 
-console.log(generateJoke());
+const laughImg = document.getElementById("laughImg");
+laughImg.src = laughing;
+
+const jokeBtn = document.getElementById("jokeBtn");
+jokeBtn.addEventListener("click", generateJoke);
+generateJoke();
